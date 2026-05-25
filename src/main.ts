14805f2381c7a568d0c2d6a87d051d986f4b5a8b
Pabitra Dakua <appudq670@gmail.com>
2026-05-25 15:54:52 +0530

    added code

--- src/main.ts
+++ src/main.ts
@@ -55,7 +55,7 @@ async function bootstrap() {
 
   
 
-  const port = process.env.PORT ?? 3001;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log("это устарелоэто устарелоэто устарелоэто устарелоэто устарело")
 
